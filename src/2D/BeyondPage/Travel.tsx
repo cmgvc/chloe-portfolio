e31aac2f5df93e6bf89630239c95169b08618e1d
cmgvc <chloe.gavv@gmail.com>
2026-03-09 23:11:37 -0400

    remove travel vid tmp

--- src/2D/BeyondPage/Travel.tsx
+++ src/2D/BeyondPage/Travel.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import StatusBadge from "../../components/StatusBadge";
 import { photos } from "../../data/travel";
 
